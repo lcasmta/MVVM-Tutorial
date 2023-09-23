5ee1e84931f18c69226a3bcee8cc3ee916e3f810
Lucas Mota <lucasmota7@gmail.com>
2023-09-23 18:40:50 -0300

    Add project files.

---
+++ MVVM Tutorial/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MVVM_Tutorial.ViewModel;
+using System.Windows;
+
+namespace MVVM_Tutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+    }
+}
---
+++ MVVM Tutorial/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using MVVM_Tutorial.Model;
+using MVVM_Tutorial.MVVM;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MVVM_Tutorial.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => SelectedItem != null);
+
+        public RelayCommand SaveCommand => new RelayCommand(execute => Save(), canExecute => canSave());
+        public MainWindowViewModel()
+        {
+        Items= new ObservableCollection<Item>();
+        }
+
+        private Item selectedItem;
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value;
+                onPropertyChanged();
+            }
+        }
+
+        private void AddItem()
+        {
+            Items.Add(new Item
+            {
+                Name = "New Item",
+                SerialNumber = "XXXX",
+                Quantity = 0
+            });
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(selectedItem);
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private bool CanSave()
+        {
+            return true;
+        }
+
+       
+    }
+}
